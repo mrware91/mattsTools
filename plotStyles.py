418ac5a0f1c388dcae893fe1cd40962559fb9f77
Matthew R Ware <mrware91@gmail.com>
2018-03-25 12:59:26 -0700

    Edited blueRedmesh

--- plotStyles.py
+++ plotStyles.py
@@ -1,15 +1,19 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-def blueRedMesh( X, Y, Z, xlabel='x', ylabel='y' ):
+def blueRedMesh( X, Y, Z, xlabel='x', ylabel='y', vmax=None):
     absmax = np.abs(Z).max()
+    if vmax is None:
+        vmax = absmax
+
     plt.pcolormesh(X,Y,Z,
-                   cmap='RdBu_r', vmin=-absmax ,vmax=absmax)
+                   cmap='RdBu_r', vmin=-vmax ,vmax=vmax)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.colorbar()
 
-def defaultMesh( X, Y, Z, xlabel='x', ylabel='y'  ):
-    plt.pcolormesh(X,Y,Z,cmap='inferno')
+def defaultMesh( X, Y, Z, xlabel='x', ylabel='y', vmin=None, vmax=None  ):
+    plt.pcolormesh(X,Y,Z,cmap='inferno',vmin=vmin, vmax=vmax)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.colorbar()
