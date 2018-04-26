96cdc27eeeac86e3eb5189ca929d1a50dedcfe08
Matthew R Ware <mrware91@gmail.com>
2018-04-26 11:31:57 -0700

    Added logPolyVal

--- tileOperations.py
+++ tileOperations.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+################################################################################
+#~~~~~~~~~Tiling
+################################################################################
 def tileLike(v,A):
     axis = 1
     if v.shape[0] == A.shape[1]:
@@ -10,15 +12,31 @@ def mean_subtract(A,axis=0):
     return A - np.tile(np.mean(A,axis=axis),(A.shape[axis],1)).reshape(A.shape)
 
 def t0_subtract(A,axis=0):
-    xslice = 0 
+    xslice = 0
     yslice = 0
-    
+
     if axis==0:
         yslice = range(A.shape[1])
     else:
         xslice = range(A.shape[0])
-        
+
     return A - np.tile(A[xslice,yslice],(A.shape[axis],1)).reshape(A.shape)
 
 def tileMultiply(v,A):
     return A*tileLike(v,A)
+
+################################################################################
+#~~~~~~~~~Array slicing
+################################################################################
+def lineout( S, Q, Q0, axis=None ):
+    idx = np.abs( Q-Q0 ).argmin()
+    if axis == None:
+        if S.shape[0] == Q.shape[0]:
+            axis = 0
+        else:
+            axis = 1
+
+    if axis == 0:
+        return S[idx,:]
+    else:
+        return S[:,idx]
